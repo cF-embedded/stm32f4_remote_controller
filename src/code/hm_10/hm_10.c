640bd7af051db94d2e0fba1cb5ef0924e8497b91
FILIPPO <filippocerta3@gmail.com>
2024-04-29 18:50:58 +0200

    hm-10: add hm-10 files

---
+++ src/code/hm_10/hm_10.c
@@ -0,0 +1,11 @@
+/**
+ * @file hm_10.c
+ * @author cF-embedded (cf@embedded.pl)
+ * @brief hm-10 driver with at commands
+ * @date 2024-04-29
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "hm_10.h"
---
+++ src/code/hm_10/hm_10.h
@@ -0,0 +1,22 @@
+/**
+ * @file hm_10.h
+ * @author cF-embedded (cf@embedded.pl)
+ * @brief hm-10 driver with at commands
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef _HM_10_H_
+#define _HM_10_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _HM_10_H_ */
