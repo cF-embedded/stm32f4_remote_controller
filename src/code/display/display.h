902f0572367fdb9683b63290e2a36b52c5baf62a
FILIPPO <filippocerta3@gmail.com>
2024-05-02 15:50:27 +0200

    display: add display files

---
+++ src/code/display/display.h
@@ -0,0 +1,15 @@
+/**
+ * @file display.h
+ * @author cF-embedded (cf@embedded.pl)
+ * @brief display header file
+ * @date 2024-04-28
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#ifndef _DISPLAY_H
+#define _DISPLAY_H
+
+void display_tasks_init(void);
+
+#endif /* _DISPLAY_H */
