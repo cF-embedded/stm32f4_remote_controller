e7ee1f71dc6a5923a97dd5b8b80c40c1c94cdf95
FILIPPO <filippocerta3@gmail.com>
2024-05-07 17:20:05 +0200

    initialization: add joystick_controller to initialization

--- src/initialization/initialization.c
+++ src/initialization/initialization.c
@@ -1,10 +1,12 @@
 #include "core_init.h"
 #include "hm_10.h"
-
+#include "joystick_controller.h"
 
 void system_init(void)
 {
     core_init();
-    
+
+    joystick_control_task_init();
+
     hm_10_task_init();
 }
