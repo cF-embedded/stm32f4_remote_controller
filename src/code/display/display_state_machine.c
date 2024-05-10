d5c6daad25f3542c290e009d916a4aac15e5aebf
FILIPPO <filippocerta3@gmail.com>
2024-05-10 21:41:57 +0200

    display: add display_state_machine files

---
+++ src/code/display/display_state_machine.c
@@ -0,0 +1,10 @@
+/**
+ * @file display_state_machine.c
+ * @author cF-embedded (cf@embedded.pl)
+ * @brief
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "display_state_machine.h"
---
+++ src/code/display/display_state_machine.h
@@ -0,0 +1,19 @@
+/**
+ * @file display_state_machine.h
+ * @author cF-embedded (cf@embedded.pl)
+ * @brief state machine for display screens
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef _DISPLAY_STATE_MACHINE_H
+#define _DISPLAY_STATE_MACHINE_H
+
+/**
+ * @brief start a state machine of displays screens
+ *
+ */
+void display_state_machine_start(void);
+
+#endif /* _DISPLAY_STATE_MACHINE_H */
