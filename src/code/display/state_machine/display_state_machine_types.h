521429e42948cf521b692ab372932bf2e05845a1
FILIPPO <filippocerta3@gmail.com>
2024-05-11 20:29:42 +0200

    display: add display_state_machine_types file

---
+++ src/code/display/state_machine/display_state_machine_types.h
@@ -0,0 +1,13 @@
+/**
+ * @file display_state_machine_types.h
+ * @author cF-embedded (cf@embedded.pl)
+ * @brief types for display state machine
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef _DISPLAY_STATE_MACHINE_TYPES_H
+#define _DISPLAY_STATE_MACHINE_TYPES_H
+
+#endif /* _DISPLAY_STATE_MACHINE_TYPES_H */
